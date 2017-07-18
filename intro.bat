f5c26b2d569a91bbf0afa5abed6c366446b2247b
Rodrigo Maestro <rodrmp@users.noreply.github.com>
2017-07-18 11:05:26 +0100

    Create intro.bat

---
+++ intro.bat
@@ -0,0 +1,58 @@
+
+@echo off
+color 69
+cls
+
+
+
+ping 192.0.2.1 -n 1 -w 22 >nul
+cls
+echo r.bat
+
+
+echo.
+echo.
+echo.   (\__/)		
+echo.   (=',')		
+echo.   (_(")(")	
+color 68
+ping 192.0.2.1 -n 1 -w 22 >nul
+color 2A
+cls	
+echo.   (\__/)			
+echo.   (=o.O)				
+echo.   C(")(")		  	
+echo.
+echo.
+ping 192.0.2.1 -n 1 -w 22 >nul
+cls
+echo.
+echo.
+echo.   (\__/)		
+echo.   (=x,X)		
+echo.   (_(")(")	
+color 35
+ping 192.0.2.1 -n 1 -w 33 >nul
+color 74
+cls
+echo.   (\__/)				 
+echo.   (=3.3)				  
+echo.   C(")(")		  	
+echo.
+echo.
+ping 192.0.2.1 -n 1 -w 22 >nul
+cls
+echo.
+echo.
+echo.   (\__/)		
+echo.   (=',')		
+echo.   (_(")(")	
+color 65
+ping 192.0.2.1 -n 1 -w 22 >nul
+color 8E
+cls	
+echo.   (\__/)			
+echo.   (=o.O)				
+echo.   C(")(")		  	
+echo.
+echo.
